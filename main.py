1e4867e374c4be4d6667ed222814d681dad87df0
emirbusra1035 <emirbusra1035@gmail.com>
2022-08-04 12:50:44 +0300

    added image_crop.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Upload Annotations folder
+# Upload Test, Train and Val folders
+# Upload create_targets.py classes.json 
+# Upload preprocess_annotations.py preprocess_images.py
+# Upload train.py utils.py
+
+# Run preprocess_annotations.py
+
+# Run preprocess_images.py
+
+# Run create_targets.py
